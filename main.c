52fc4db6cb66fb818463d1796f934fd653e1ea52
JomanaAshraf <jomanaashraf8@gmail.com>
2020-11-07 10:53:29 +0200

    test

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int *pc,c,x;
+    int arr[3]={1,2,3};
+    c=5;
+    pc=&c;
+    printf("%d\n",c);
+    printf("%d\n",*pc);
+    printf("%llx\n",&c);
+    printf("%p\n",pc);
+    *pc=10;
+    printf("%d",c);
+  //  x=arr;
+    //printf("%d",x);
+
+}
